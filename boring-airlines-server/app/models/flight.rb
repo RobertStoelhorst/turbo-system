c7f9f0e07b388f9584fcb1e776727c08f710eb04
ROBERT_STOELHORST <robbiestoelhorst@hotmail.com>
2020-11-03 20:17:53 +1100

    .rb

--- boring-airlines-server/app/models/flight.rb
+++ boring-airlines-server/app/models/flight.rb
@@ -1,2 +1,5 @@
 class Flight < ApplicationRecord
+  has_many :users, through: :reservations
+  has_many :reservations
+  belongs_to :plane
 end
--- boring-airlines-server/app/models/plane.rb
+++ boring-airlines-server/app/models/plane.rb
@@ -1,2 +1,4 @@
 class Plane < ApplicationRecord
+  has_many :flights
+  has_many :users
 end
