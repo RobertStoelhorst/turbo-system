9bd4f5cd47a4af16450c16a1bb60ce7ee8e11bf6
Carla Rudder <CarlaRudder@WilliamRudder.local>
2020-11-03 20:27:30 +1100

    update

--- boring-airlines-server/app/models/user.rb
+++ boring-airlines-server/app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
+  has_secure_password
+validates :username, presence: true
+validates :username, uniqueness: true
+validates :username, length: { minimum: 4 }
+validates :email, presence: true
+validates :email, uniqueness: true
+validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
   has_many :flights
   has_and_belongs_to_many :flights
+
+
+
 end
