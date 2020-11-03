f41eb9e3679a9bf8c07d17faf0c0216a7bef4cc1
Ryan Bullough <rjbullough@gmail.com>
2020-11-03 19:15:45 +1100

    Initial commit for Front End

---
+++ boring-airlines/src/components/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import Routes from "../Routes";
+import "../App.css";
+
+function App() {
+  return (
+    <div className="App container">
+      <Navbar fluid collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Boring Airlines</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem href="/signup">Signup</NavItem>
+            <NavItem href="/login">Login</NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
---
+++ boring-airlines/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Boring Airlines</h1>
+        <p>A simple flight booking app</p>
+      </div>
+    </div>
+  );
+}
