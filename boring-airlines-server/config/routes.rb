fc2490086467a5460293d50565b5aa40593f9865
Carla Rudder <CarlaRudder@WilliamRudder.local>
2020-11-04 10:36:12 +1100

    update

--- boring-airlines-server/config/routes.rb
+++ boring-airlines-server/config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
   resources :flights
   resources :planes
-<<<<<<< HEAD
+
 
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/logged_in', to: 'sessions#is_logged_in?'
-  
+
   resources :users, only: [:create, :show, :index]
-=======
-   resources :users, only: [:create, :show, :index]
->>>>>>> 6eae872bd25d93d6fe4ccda611e5c5b0f4074c59
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
